2da6bdd18e32e4a68e83e823ada13584ce4e3088
Alexander Savinykh <suineg@inbox.ru>
2019-10-18 20:06:57 +0500

    Create quick_start.bash

---
+++ quick_start.bash
@@ -0,0 +1,21 @@
+# change password
+passwd
+
+
+# update and upgrade system
+apt-get update && apt-get upgrade
+
+
+# install GUI
+sudo apt install xfce4
+
+# start GUI
+sudo startx
+
+
+# SVN
+sudo apt install subversion
+
+# Change timezone
+sudo dpkg-reconfigure tzdata
+date
