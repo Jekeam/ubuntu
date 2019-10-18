8b0d25b0708b0248320f5f63ace8f05a84805978
Alexander Savinykh <suineg@inbox.ru>
2019-10-18 20:23:54 +0500

    Update quick_start.bash

--- quick_start.bash
+++ quick_start.bash
@@ -2,20 +2,26 @@
 passwd
 
 
+
 # update and upgrade system
 apt-get update && apt-get upgrade
 
 
+
 # install GUI
 sudo apt install xfce4
-
 # start GUI
 sudo startx
 
 
+
 # SVN
 sudo apt install subversion
-
 # Change timezone
 sudo dpkg-reconfigure tzdata
 date
+
+
+
+# Remove directory with files
+rm -r folder_name
