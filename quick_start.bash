c4a1ef4305a15dd084366413172f74e020cc9d4d
Alexander Savinykh <suinegne@gmail.com>
2020-01-22 09:37:58 +0300

    Update quick_start.bash

--- quick_start.bash
+++ quick_start.bash
@@ -96,6 +96,7 @@ sudo apt install firefox
 sudo apt install snapd
 
 # git ~/.ssh https://help.github.com/en/github/authenticating-to-github/checking-for-existing-ssh-keys
+cd ~/.ssh/
 ssh-keygen -t rsa -b 4096 -C "your@email"
 ls -al
 eval $(ssh-agent -s)
