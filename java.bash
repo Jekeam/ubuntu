21476bbbed3c98cc667a60447b58b922df164c73
Alexander Savinykh <suinegne@gmail.com>
2020-02-06 12:54:19 +0300

    Create java.bash

---
+++ java.bash
@@ -0,0 +1,7 @@
+Linux: 1. Установка JDK
+
+--Во-первых, обновим индекс пакетов.
+sudo apt-get update
+
+--Далее, установим Java. В частности, эта команда устанавливает среду выполнения Java (JDK - Java Development Kit).
+sudo apt-get install default-jdk
