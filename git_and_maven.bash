a19a25d9db50c757381d0403f9fc11b67ffc7e75
Alexander Savinykh <suinegne@gmail.com>
2020-02-19 15:39:06 +0300

    Update git_and_maven.bash

--- git_and_maven.bash
+++ git_and_maven.bash
@@ -1,3 +1,13 @@
 # Для установки Git в ОС Linux вам необходимо выполнить следующие команды:
 sudo apt-get install maven git
 mvn -version
+
+# GIT (example)
+# rollback commit in repo
+ git push -f origin bb1027939c166a7c975b6b7ce9127a7bcfbd916f:scan
+ # delete last commit and keep changes
+ git reset bb1027939c166a7c975b6b7ce9127a7bcfbd916f 
+ # add new commit
+ # git add bot.py cupon.py forks.py run.py server.py util_olimp.py util_pinnacle.py util_fonbet.py proxy_worker.py utils.py 
+ # git commit -m 'hend merge pinancle' 
+ # git push
